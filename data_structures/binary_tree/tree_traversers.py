c9713a0d701dd1470b8622b91871f665d740fa5b
JinLisek <tastyramen@gmail.com>
2022-01-07 14:45:43 +0100

    Add traverse tree preorder

--- data_structures/binary_tree/tree_traversers.py
+++ data_structures/binary_tree/tree_traversers.py
@@ -10,3 +10,12 @@ def traverse_tree_inorder(node: Optional[Node], result: List[int]) -> None:
     traverse_tree_inorder(node.left, result)
     result.append(node.value)
     traverse_tree_inorder(node.right, result)
+
+
+def traverse_tree_preorder(node: Optional[Node], result: List[int]) -> None:
+    if node is None:
+        return
+
+    result.append(node.value)
+    traverse_tree_preorder(node.left, result)
+    traverse_tree_preorder(node.right, result)
