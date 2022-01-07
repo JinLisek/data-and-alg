b86a5932c68de98da4662daaa939245ac3ae5516
JinLisek <tastyramen@gmail.com>
2022-01-07 15:21:28 +0100

    Rename tree to object_under_test in tests

--- data_structures/binary_tree/tests/test_binary_search_tree.py
+++ data_structures/binary_tree/tests/test_binary_search_tree.py
@@ -10,10 +10,10 @@ def test_given_empty_tree_then_is_empty_should_return_true():
 
 
 def test_given_empty_tree_then_is_empty_should_return_false():
-    tree = BinarySearchTree()
-    tree.insert(15)
+    object_under_test = BinarySearchTree()
+    object_under_test.insert(15)
 
-    assert not tree.is_empty()
+    assert not object_under_test.is_empty()
 
 
 @pytest.mark.parametrize(
@@ -28,12 +28,12 @@ def test_given_empty_tree_then_is_empty_should_return_false():
 def test_given_elements_then_traverse_inorder_should_return_them_in_order(
     input_elements, expected_output
 ):
-    tree = BinarySearchTree()
+    object_under_test = BinarySearchTree()
 
     for ele in input_elements:
-        tree.insert(value=ele)
+        object_under_test.insert(value=ele)
 
-    assert expected_output == tree.traverse_inorder()
+    assert expected_output == object_under_test.traverse_inorder()
 
 
 @pytest.mark.parametrize(
@@ -48,46 +48,46 @@ def test_given_elements_then_traverse_inorder_should_return_them_in_order(
 def test_given_elements_then_traverse_preorder_should_return_elements_in_preorder_with_parents_first(
     input_elements, expected_output
 ):
-    tree = BinarySearchTree()
+    object_under_test = BinarySearchTree()
 
     for ele in input_elements:
-        tree.insert(value=ele)
+        object_under_test.insert(value=ele)
 
-    assert expected_output == tree.traverse_preorder()
+    assert expected_output == object_under_test.traverse_preorder()
 
 
 def test_inserting_duplicated_value_should_raise_an_error():
-    tree = BinarySearchTree()
+    object_under_test = BinarySearchTree()
 
     duplicated_elem = 8
-    tree.insert(duplicated_elem)
+    object_under_test.insert(duplicated_elem)
 
     with pytest.raises(DuplicatedValueInBSTError) as err:
-        tree.insert(duplicated_elem)
+        object_under_test.insert(duplicated_elem)
 
     assert f"{duplicated_elem}" in str(err)
 
 
 def test_search_on_empty_tree_returns_none():
-    tree = BinarySearchTree()
+    object_under_test = BinarySearchTree()
 
-    assert tree.search(8) is None
+    assert object_under_test.search(8) is None
 
 
 def test_search_on_a_tree_containing_given_value_should_return_node_with_the_value():
-    tree = BinarySearchTree()
-    tree.insert(8)
+    object_under_test = BinarySearchTree()
+    object_under_test.insert(8)
 
-    result = tree.search(8)
+    result = object_under_test.search(8)
 
     assert result.value == 8
 
 
 def test_searching_for_a_value_which_node_is_not_a_leaf_should_return_node_pointing_to_other_node():
-    tree = BinarySearchTree()
-    tree.insert(2)
-    tree.insert(8)
+    object_under_test = BinarySearchTree()
+    object_under_test.insert(2)
+    object_under_test.insert(8)
 
-    result = tree.search(2)
+    result = object_under_test.search(2)
 
     assert result.right.value == 8
