4ab609feb6083c8db1ad7e71bdeb99ac66facb49
JinLisek <tastyramen@gmail.com>
2022-01-30 19:14:18 +0100

    Add ColorNode

--- data_structures/binary_tree/node.py
+++ data_structures/binary_tree/node.py
@@ -9,3 +9,15 @@ class Node:
     value: int
     left: Optional[Node] = None
     right: Optional[Node] = None
+
+
+@dataclass
+class ColorNode:
+    value: int
+    is_red: bool
+    left: Optional[ColorNode] = None
+    right: Optional[ColorNode] = None
+    parent: Optional[ColorNode] = None
+
+    def is_uncle_black(self) -> bool:
+        return True
