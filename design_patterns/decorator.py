40e17d1d100cb635093b97ceedafbc0e8ac02a0d
JinLisek <tastyramen@gmail.com>
2022-02-26 12:33:26 +0100

    Add decorator to design patterns

---
+++ design_patterns/decorator.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+
+class Speakers(ABC):
+    @abstractmethod
+    def play(self, music: str) -> None:
+        pass
+
+
+class RegularSpeakers(Speakers):
+    def play(self, music: str) -> None:
+        print(f"Playing some {music}")
+
+
+class TimedSpeakers(Speakers):
+    def __init__(self, speakers: Speakers):
+        self.__decorated = speakers
+
+    def play(self, music: str) -> None:
+        print("Waiting 3 seconds")
+        self.__decorated.play(music=music)
+
+
+def play_rock_and_roll(speakers: Speakers) -> None:
+    speakers.play(music="Rock & Roll")
+
+
+if __name__ == "__main__":
+    play_rock_and_roll(speakers=RegularSpeakers())
+
+    print("=" * 100)
+
+    play_rock_and_roll(speakers=TimedSpeakers(speakers=RegularSpeakers()))
